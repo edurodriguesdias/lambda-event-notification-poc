861e531ce13299aec67436924be959ed7f2ed2c5
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2024-02-12 16:22:10 -0300

    reading file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
-import type { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Context } from 'aws-lambda'
+import type { S3Event, Context } from 'aws-lambda'
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { sdkStreamMixin } from '@aws-sdk/util-stream-node';
 
-type LambdaFunctionUrlEvent = APIGatewayProxyEventV2
+const s3 = new S3Client({ });
 
-export const handler = async (event: LambdaFunctionUrlEvent, context: Context) => {
-    console.log('Event received', event)
-    console.log('Context', context)
+export const handler = async (event: S3Event, context: Context) => {
+
+    const getObjectRequests = event.Records.map(record => {
+
+        const objectParams = {
+          Bucket: record.s3.bucket.name,
+          Key: record.s3.object.key
+        };
+    
+        const getObject = new GetObjectCommand(objectParams);
+    
+        return s3.send(getObject).then((data) => 
+          sdkStreamMixin(data.Body).transformToString().then((objectString: string) => {
+            console.info(objectString);
+            return Promise.resolve(objectString);
+          })
+          .catch((err: any) => {
+           console.error("Error consuming response stream:", err);
+           return Promise.reject(err);
+          })
+          )
+          .catch((err) => {
+          console.error("Error to get Object", err);
+          return Promise.reject(err);
+        })
+      });
+
+      return Promise.all(getObjectRequests).then(() => {
+        console.debug('File already processed!');
+      });
 }
