8d39dcb2f2759d371a9c370909f55d2eebde65ae
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2024-02-24 02:27:44 -0300

    init unit tests

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+export default {
+  testEnvironment: 'node',
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.m?[tj]s?$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.(m)?js$': '$1',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(m)?ts$',
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    'src/**/*.mts',
+    '!src/**/*.d.ts',
+    '!src/**/*.d.mts',
+  ],
+  coverageReporters: ["json", "html"]
+};
